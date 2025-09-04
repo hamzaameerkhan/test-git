9ecf43bcbfb64f2a4e5a87e82ef7c99c6b28d990
hamzaameerkhan <hkehtran2@gmail.com>
2025-09-04 10:31:23 -0400

    asa

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "example" {
+  length = 2
+}
+
+output "pet_name" {
+  value = random_pet.example.id
+}
+
+
